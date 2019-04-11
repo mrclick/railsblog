faa72295b494636f3e833ea5b31646984f22c52b
JP Ngabonziza <jeanpierren@gmail.com>
2019-04-11 23:32:57 +0200

    Fix create, add flash/validation and error messages, create show

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+    #Email Validation
+    #validates :title, format: { with: URI::MailTo::EMAIL_REGEXP, message: "only allows valid emails" }
+
     validates :title, presence: true, length: { minimum:3, maximum: 50}
     validates :description, presence: true, length: { minimum:10, maximu: 300}
 end
