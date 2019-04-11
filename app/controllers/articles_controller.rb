a6777b8b5a4479c6ed3d9196c80d0269e7d1590d
JP Ngabonziza <jeanpierren@gmail.com>
2019-04-12 00:36:01 +0200

    Add edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,10 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def index
+        @articles = Article.all
+    end
+
     def create
         
         @article = Article.new(article_params)
