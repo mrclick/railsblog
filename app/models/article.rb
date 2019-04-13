d3732af6e86eef44d193448aeb9a801126486b46
JP Ngabonziza <jeanpierren@gmail.com>
2019-04-13 19:41:53 +0200

    Add association between articles and user and add debuger to view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
+    belongs_to :user
     #Email Validation
     #validates :title, format: { with: URI::MailTo::EMAIL_REGEXP, message: "only allows valid emails" }
 
     validates :title, presence: true, length: { minimum:3, maximum: 50}
     validates :description, presence: true, length: { minimum:10, maximu: 300}
+    validates :user_id, presence: true
 end
